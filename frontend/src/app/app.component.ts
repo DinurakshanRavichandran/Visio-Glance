f67e34992bed5a9891c73b891e1a42b116f24883
DinurakshanRavichandran <dinurakshan@gmail.com>
2025-03-03 22:15:33 +0530

    This commit added the page to the login and set the login page as the main webpage and also set up the design for a basic closed web application

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, RouterModule],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
