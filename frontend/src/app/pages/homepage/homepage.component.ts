781b329bd38eca37aba86bc1125fdc4d5b7a29ae
DinurakshanRavichandran <dinurakshan@gmail.com>
2025-03-09 21:37:39 +0530

    Fixed the home page error

--- frontend/src/app/pages/homepage/homepage.component.ts
+++ frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
