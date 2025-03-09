837be2901e01868684c0ed49a6a5f91369be9507
DinurakshanRavichandran <dinurakshan@gmail.com>
2025-03-10 00:22:06 +0530

    Fixed and configured json file

--- frontend/src/app/pages/system/system.component.ts
+++ frontend/src/app/pages/system/system.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-system',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './system.component.html',
   styleUrl: './system.component.css'
 })
